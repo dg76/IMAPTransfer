4d1ce0c63a98ff7defbb4ad382b694ce229641f1
dom <contact@dgunia.de>
2023-07-27 19:34:50 +0200

    Bug fix for the reconnection problem

--- src/main/kotlin/com/dgunia/imaptransfer/WatchNoopThread.kt
+++ src/main/kotlin/com/dgunia/imaptransfer/WatchNoopThread.kt
@@ -9,11 +9,11 @@ import javax.mail.MessagingException
 /**
  * Runs the IMAP NOOP command every five minutes to ensure that the IMAP connection is not closed. Reconnects if necessary.
  */
-class WatchNoopThread(var imapFolder: IMAPFolder, val reconnect: () -> IMAPFolder) : Runnable {
+class WatchNoopThread(val imapFolder: IMAPFolder, val reconnect: () -> IMAPFolder) : Runnable {
     override fun run() {
         val lastReconnect = Date();
-        while (!Thread.interrupted()) {
-            try {
+        try {
+            while (!Thread.interrupted()) {
                 Thread.sleep(5 * 60 * 1000)
 
                 // Perform a NOOP to keep the connection alive
@@ -27,23 +27,23 @@ class WatchNoopThread(var imapFolder: IMAPFolder, val reconnect: () -> IMAPFolde
                 if (lastReconnect.toInstant().plus(Duration.ofHours(1)).isAfter(Date().toInstant())) {
                     Logger.getGlobal().warning("Reconnect source after an hour");
                     imapFolder.close()
-                    imapFolder = reconnect()
-                    return
+                    reconnect()
+                    break
                 }
-            } catch (e: InterruptedException) {
-                break
-            } catch (e: MessagingException) {
-                Logger.getGlobal().warning("Unexpected IMAP IDLE exception " + e.localizedMessage)
-
-                imapFolder = reconnect()
-            } catch (e: Exception) {
-                Logger.getGlobal().warning("NOOP thread exception " + e.localizedMessage)
-                e.printStackTrace()
-
-                Thread.sleep(1 * 60 * 1000) // wait a minute then try to connect again
-
-                imapFolder = reconnect()
             }
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        } catch (e: MessagingException) {
+            Logger.getGlobal().warning("Unexpected IMAP IDLE exception " + e.localizedMessage)
+
+            reconnect()
+        } catch (e: Exception) {
+            Logger.getGlobal().warning("NOOP thread exception " + e.localizedMessage)
+            e.printStackTrace()
+
+            Thread.sleep(1 * 60 * 1000) // wait a minute then try to connect again
+
+            reconnect()
         }
     }
 }
